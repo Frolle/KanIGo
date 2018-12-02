66fd63c5fb30b0651eaf1bae2a7ae759c3674a5f
Simon Fransson <simon.fransson@layer10.se>
2018-12-02 18:53:31 +0100

    Initial design.

---
+++ app/src/main/java/se/layer10/kanigo/MainActivity.java
@@ -0,0 +1,13 @@
+package se.layer10.kanigo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
