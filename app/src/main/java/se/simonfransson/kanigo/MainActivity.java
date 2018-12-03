7e38fd480fa00f9129ac6310a8da7e80ae50da2c
Simon Fransson <simon.fransson@layer10.se>
2018-12-03 17:38:31 +0100

    Changed to use Handler and Runnable instead of CountDownTimer.

--- app/src/main/java/se/simonfransson/kanigo/MainActivity.java
+++ app/src/main/java/se/simonfransson/kanigo/MainActivity.java
@@ -1,7 +1,7 @@
 package se.simonfransson.kanigo;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,35 +24,34 @@ public class MainActivity extends AppCompatActivity {
         final Button checkInButton = (Button) view;
         final ConstraintLayout rootLayout = findViewById(R.id.rootLayout);
         final TextView timeUntil = findViewById(R.id.timeUntilTextView);
-        final TextView descriptText = findViewById(R.id.answerTextView);
+        final TextView descriptionText = findViewById(R.id.answerTextView);
 
         checkInButton.setEnabled(false);
         checkInButton.setClickable(false);
         rootLayout.setBackgroundColor(getResources().getColor(R.color.bestRed, null));
-        descriptText.setText(R.string.time_to_wait);
+        descriptionText.setText(R.string.time_to_wait);
 
-        LocalDateTime now = LocalDateTime.now(); // sparas ned till databas
-        final LocalDateTime whenToStop = now.plusSeconds(10);
-        Duration between = Duration.between(now, whenToStop);
-        long millis = between.toMillis();
+        final LocalDateTime whenToStop = LocalDateTime.now().plusSeconds(10);
 
-        new CountDownTimer(millis, 1000) {
+        final Handler timerHandler = new Handler();
+        Runnable tickRunnable = new Runnable() {
             @Override
-            public void onTick(long l) {
-                Duration timeLeft = Duration.between(LocalDateTime.now(), whenToStop);
-                String dateText = formatDateString(timeLeft);
-                timeUntil.setText(dateText);
+            public void run() {
+                if (LocalDateTime.now().isAfter(whenToStop)) {
+                    rootLayout.setBackgroundColor(getResources().getColor(R.color.bestGreen, null));
+                    checkInButton.setEnabled(true);
+                    checkInButton.setClickable(true);
+                    timeUntil.setText(null);
+                    descriptionText.setText(R.string.time_to_go);
+                } else {
+                    Duration timeLeft = Duration.between(LocalDateTime.now(), whenToStop);
+                    String dateText = formatDateString(timeLeft);
+                    timeUntil.setText(dateText);
+                    timerHandler.postDelayed(this, 1000);
+                }
             }
-
-            @Override
-            public void onFinish() {
-                rootLayout.setBackgroundColor(getResources().getColor(R.color.bestGreen, null));
-                checkInButton.setEnabled(true);
-                checkInButton.setClickable(true);
-                timeUntil.setText(null);
-                descriptText.setText(R.string.time_to_go);
-            }
-        }.start();
+        };
+        timerHandler.post(tickRunnable);
     }
 
     private String formatDateString(Duration timeLeft) {
